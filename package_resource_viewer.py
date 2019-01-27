1335ed9593fde2faeb550d7871355bd799a2863d
Maximilian Heinzler <m.heinzler@heinzler.de>
2019-01-27 16:39:48 +0100

    PackageResourceViewer: Switch to the new patch method

--- package_resource_viewer.py
+++ package_resource_viewer.py
@@ -1,14 +1,30 @@
 """Improve viewing package resources."""
 
-from PackageResourceViewer import package_resource_viewer
+import sublime_plugin
+
+from .tools.patch import *
 
 
-class PackageResourceViewerCommand(
-        package_resource_viewer.PackageResourceViewerCommand):
-    """Overwrite the insert_text method."""
+def plugin_loaded():
+    """Apply patches."""
 
-    def insert_text(self, content, view):
+    # Force a reload of the plugin and do the patching here. The
+    # package_resources module reloads the package_resource_viewer module on
+    # the first start which would otherwise mess with the patching.
+    sublime_plugin.reload_plugin(
+        "PackageResourceViewer.package_resource_viewer")
+    from PackageResourceViewer import package_resource_viewer
+
+    @patch(package_resource_viewer.PackageResourceViewerBase)
+    def insert_text(patch, self, content, view):
         """Call the original method and detect the indentation."""
-        super().insert_text(content, view)
+        patch.original(self, content, view)
         if not view.is_loading():
             view.run_command("detect_indentation")
+
+    apply_patches(__name__)
+
+
+def plugin_unloaded():
+    """Restore patches."""
+    restore_patches(__name__)
