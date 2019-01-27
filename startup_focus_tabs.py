11bd95f62dcc95bf2bba8aca70ce87aea40e33a2
Maximilian Heinzler <m.heinzler@heinzler.de>
2019-01-27 14:23:38 +0100

    Focus tabs only once

--- startup_focus_tabs.py
+++ startup_focus_tabs.py
@@ -3,16 +3,26 @@
 import sublime
 
 
+try:
+    first_run # noqa
+except NameError:
+    first_run = True
+
+
 def plugin_loaded():
     """Focus the active view in the tabs of all windows."""
-    for window in sublime.windows():
-        # save the active view and focus on it later to not change the active
-        # group
-        active_view = window.active_view()
+    global first_run
+    if first_run:
+        for window in sublime.windows():
+            # save the active view and focus on it later to not change the
+            # active group
+            active_view = window.active_view()
 
-        for group in range(window.num_groups()):
-            # setting the index again forces the tabs to focus on the view
-            view = window.active_view_in_group(group)
-            window.set_view_index(view, *window.get_view_index(view))
+            for group in range(window.num_groups()):
+                # setting the index again forces the tabs to focus on the view
+                view = window.active_view_in_group(group)
+                window.set_view_index(view, *window.get_view_index(view))
 
-        window.focus_view(active_view)
+            window.focus_view(active_view)
+
+        first_run = False
