6a84bb253ede5a8dd7b17c4443cd1f2d6b91505c
Maximilian Heinzler <m.heinzler@heinzler.de>
2019-03-11 20:30:07 +0100

    ContextMenuCursor: Fix flake8 errors

--- context_menu_cursor.py
+++ context_menu_cursor.py
@@ -1,10 +1,16 @@
-import sublime
+"""Context menu command that also positions the cursor."""
+
 import sublime_plugin
 
-class ContextMenuCursorCommand(sublime_plugin.TextCommand):
-	def run(self, edit, event, *args):
-		self.view.run_command("drag_select", { "event": event })
-		self.view.run_command("context_menu", { "event": event })
 
-	def want_event(self):
-		return True
+class ContextMenuCursorCommand(sublime_plugin.TextCommand):
+    """Context menu command that also positions the cursor."""
+
+    def run(self, edit, event, *args):
+        """Run the command."""
+        self.view.run_command("drag_select", {"event": event})
+        self.view.run_command("context_menu", {"event": event})
+
+    def want_event(self):
+        """Whether we need the event data."""
+        return True
